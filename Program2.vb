4c568926315f06232403c2003d01d4890c90cb2a
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2023-03-16 09:49:07 +0300

    Add files via upload

--- Program2.vb
+++ Program2.vb
@@ -5,12 +5,19 @@ Module Program
 
         '//TODO: Генерируем код
         Dim ver As String
-        Dim namber As String = "1234"
+
+        Dim namber As String
+        Randonize()
+        namber = Int((9000 * Rnd() + 1000)
+
+        Console.WriteLine(namber)
+        Console.WriteLine(namber)
+
         Dim cow As Integer = 0
         Dim bull As Integer = 0
         Dim Rgh As Integer
 
-        Console.WriteLine("Привет, попробуй угадать загаданное число!(Введи число)")
+        Console.WriteLine("Привет, попробуй угадать загаданное число!")
 
         Do
             Console.WriteLine("Введи число")
@@ -22,10 +29,8 @@ Module Program
                     For i As Integer = 0 To 3
                         If ver.Chars(m) = namber.Chars(i) Then
                             If i = m Then
-                                Console.WriteLine("{0} цыфра вашего слова - бык", m + 1)
                                 bull = bull + 1
                             Else
-                                Console.WriteLine("{0} цыфра вашего слова - корова", m + 1)
                                 cow = cow + 1
                             End If
                         End If
@@ -33,17 +38,20 @@ Module Program
                 Next
                 Rgh = bull + cow
                 Console.WriteLine("В вашем числе {0} быков и {1} коров, всего правильных ответов {0}", bull, cow, Rgh)
+                bull = 0
+                cow = 0
+
             Loop Until namber = ver
         Loop
         Console.WriteLine("Число не четырехзначное или же в нем повторяются цифры, такое вводить нельзя!")
 
 
     End Sub
-    Function ChackDublicates(ver As String) As Boolean
+    Function ChackDublicates(num As String) As Boolean
         ChackDublicates = False
-        For n As Integer = 0 To 2
-            For i As Integer = n + 1 To 3
-                If ver.Chars(n) = ver.Chars(i) Then
+        For n As Integer = 1 To 3
+            For i As Integer = (n - 1) To 3
+                If num.Chars(n) = num.Chars(i) Then
                     ChackDublicates = True
                 End If
             Next
