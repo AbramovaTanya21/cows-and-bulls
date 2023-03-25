2f900eaa751b4ba923c70e41e26573f73f735e53
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2023-03-25 17:20:42 +0300

    Add files via upload

--- Program(Cow and Bull).vb
+++ Program(Cow and Bull).vb
@@ -22,25 +22,35 @@ Module Program
         Do
             Console.WriteLine("Введи число")
             ver = Console.ReadLine()
-            If IsCorrect(ver) Then
-                For m As Integer = 0 To 3
-                    For i As Integer = 0 To 3
-                        If ver.Chars(m) = namber.Chars(i) Then
-                            If i = m Then
-                                bull += 1
-                            Else
-                                cow += 1
-                            End If
-                            Rgh = bull + cow
-                        End If
-                    Next
-                Next
-                Console.WriteLine("В вашем числе {0} быков и {1} коров, всего правильных ответов {2}, за {3} ходов", bull, cow, Rgh, Hod)
-                bull = 0
-                cow = 0
-                Rgh = 0
+
+            If CorrectNum(ver) = False Then
+                If IsCorrect(ver) Then
+                    If ChackDublicates1(ver) = False Then
+
+                        For m As Integer = 0 To 3
+                            For i As Integer = 0 To 3
+                                If ver.Chars(m) = namber.Chars(i) Then
+                                    If i = m Then
+                                        bull += 1
+                                    Else
+                                        cow += 1
+                                    End If
+                                    Rgh = bull + cow
+                                End If
+                            Next
+                        Next
+                        Console.WriteLine("В вашем числе {0} быков и {1} коров, всего правильных ответов {2}, за {3} ход", bull, cow, Rgh, Hod + 1)
+                        bull = 0
+                        cow = 0
+                        Rgh = 0
+                    Else
+                        Console.WriteLine("Цифры в числе не должны повторяться!")
+                    End If
+                Else
+                    Console.WriteLine("Вы выели не четырехзначное число")
+                End If
             Else
-                Console.WriteLine("Вы выели не четырехзначное число")
+                Console.WriteLine("Введите,пожалуйста, число")
             End If
             Hod += 1
         Loop Until ver = namber
@@ -63,6 +73,26 @@ Module Program
         Else
             IsCorrect = False
         End If
+    End Function
+    Function ChackDublicates1(ver As String) As Boolean
+        ChackDublicates1 = False
+        For n As Integer = 1 To 3
+            For i As Integer = n To 3
+                If ver.Chars(n - 1) = ver.Chars(i) Then
+                    ChackDublicates1 = True
+                    Exit Function
+                End If
+            Next
+        Next
+    End Function
+    Function CorrectNum(ver As String) As Boolean
+        For i = 0 To ver.Length - 1
+            If Asc(ver.Chars(i)) = 49 Or 50 Or 51 Or 52 Or 53 Or 54 Or 55 Or 56 Or 57 Then
+                CorrectNum = True
+            Else
+                CorrectNum = False
+            End If
+        Next
     End Function
 End Module
 
