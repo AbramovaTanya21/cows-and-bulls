a3c0f3ac243515f4064cc050c91c33bd42a939ed
AbramovaTanya21 <123319691+AbramovaTanya21@users.noreply.github.com>
2023-04-24 21:24:39 +0300

    Add files via upload

--- Program(Cow and Bull).vb
+++ Program(Cow and Bull).vb
@@ -1,8 +1,17 @@
 ﻿Imports System
 Imports System.ComponentModel
+Imports Cows_and_bulls_5.Program
 
 Module Program
     Const Lent As Byte = 5
+
+    Enum CorrectType
+        Incorreclength
+        IncorrectName
+        duplication
+        Correctly
+        Withoutinput
+    End Enum
     Sub Main(args As String())
 
         Dim ver As String
@@ -11,6 +20,8 @@ Module Program
         Dim bull As Integer = 0
         Dim Rgh As Integer
         Dim Hod As Integer
+        Dim Mistake As CorrectType
+
 
         Dim k As ConsoleKeyInfo
         Randomize()
@@ -27,31 +38,36 @@ Module Program
                 Console.WriteLine("Введи число")
                 ver = Console.ReadLine()
 
+                Mistake = IsCorrect(ver)
+                Select Case Mistake
+                    Case CorrectType.Withoutinput : Console.WriteLine("Не время отчаиваться!")
+                    Case CorrectType.Incorreclength : Console.WriteLine("Число не той длинны!")
+                    Case CorrectType.IncorrectName : Console.WriteLine("Это разве число!?")
+                    Case CorrectType.duplication : Console.WriteLine("Цифры в числе не должны повторяться!")
+                End Select
 
-                If IsCorrect(ver) Then
-                    For m As Integer = 0 To Lent - 1
-                        For i As Integer = 0 To Lent - 1
-                            If ver.Chars(m) = namber.Chars(i) Then
-                                If i = m Then
-                                    bull += 1
-                                Else
-                                    cow += 1
-                                End If
-                                Rgh = bull + cow
+                If Mistake <> CorrectType.Correctly Then Continue Do
+
+                For m As Integer = 0 To Lent - 1
+                    For i As Integer = 0 To Lent - 1
+                        If ver.Chars(m) = namber.Chars(i) Then
+                            If i = m Then
+                                bull += 1
+                            Else
+                                cow += 1
                             End If
-                        Next
+                            Rgh = bull + cow
+                        End If
                     Next
-                    Console.WriteLine("В вашем числе {0} быков и {1} коров, всего правильных ответов {2}, за {3} ход", bull, cow, Rgh, Hod + 1)
-                    bull = 0
-                    cow = 0
-                    Rgh = 0
-                Else
-                    Console.WriteLine("Введено не коректное число!")
-                End If
-
+                Next
                 Hod += 1
+                Console.WriteLine("В вашем числе {0} быков и {1} коров, всего правильных ответов {2}, за {3} ход", bull, cow, Rgh, Hod + 1)
+                bull = 0
+                cow = 0
+                Rgh = 0
             Loop Until ver = namber
-            Console.WriteLine("Молодец. Ты угадал за {0} ходов", Hod)
+
+            Console.WriteLine("Молодец. Ты угадал за {0} ходов", Hod + 1)
             Hod = 0
             Console.WriteLine()
             Console.WriteLine("Хочешь сыграть еще раз? Нажми:(y(да),n(нет))")
@@ -75,19 +91,21 @@ Module Program
             Next
         Next
     End Function
-    Function IsCorrect(ver As String) As Boolean
-        If Len(ver) <> Lent Then Return False
 
+    Function IsCorrect(ver As String) As CorrectType
+        IsCorrect = CorrectType.Correctly
+
+        If Len(ver) = 0 Then Return CorrectType.Withoutinput
+        For i = 0 To Len(ver) - 1
+            If Not Char.IsDigit(ver.Chars(i)) Then Return CorrectType.IncorrectName
+        Next
+        If ChackDublicates(ver) Then Return CorrectType.duplication
+        If Len(ver) <> Lent Then Return CorrectType.Incorreclength
         'Try
         '    Dim Test As Integer = CInt(ver)
         'Catch ex As Exception
         '    Return False
         'End Try
-
-        For i = 0 To Len(ver) - 1
-            If Not Char.IsDigit(ver.Chars(i)) Then Return False
-        Next
-        IsCorrect = Not ChackDublicates(ver)
     End Function
 End Module
 
